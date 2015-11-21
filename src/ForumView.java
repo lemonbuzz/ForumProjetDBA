477c1c1003b712154c04a51461861f1eb888bae6
alexandre <alex.boulay1@gmail.com>
2015-11-21 18:50:55 -0500

    yes panels work with scrolling

--- src/ForumView.java
+++ src/ForumView.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.GroupLayout.Alignment;
 
 /**
@@ -14,10 +18,7 @@ public class ForumView extends JFrame {
 
 
     private int tempVAR_NbMessages = 10;
-    private JPanel panel_test;
-    private JLabel labelTitreForum;
-    private JLabel labelNbMsg;
-    private JLabel lblDerniermessage;
+
     private JButton btnNewButton;
     private JButton btnNewButton_1;
     private JButton btnNewButton_2;
@@ -33,8 +34,12 @@ public class ForumView extends JFrame {
     private JButton btnNewButton_12;
     private JButton btnNewButton_13;
 
+
+    private Ecouteur ec;
+
     public ForumView(Forum forum) {
         this.forum = forum;
+        ec = new Ecouteur();
 
         setTitle("CVM - FORUM");
         setBounds(100, 100, 896, 475);
@@ -55,79 +60,84 @@ public class ForumView extends JFrame {
         scrollPane.setViewportView(panel);
         panel.setLayout(new GridLayout(0, 1, 0, 0));
 
-        btnNewButton = new JButton("New button");
-        panel.add(btnNewButton);
 
-        btnNewButton_1 = new JButton("New button");
-        panel.add(btnNewButton_1);
+        for ( int i = 0; i < 50; i++) {
 
-        btnNewButton_2 = new JButton("New button");
-        panel.add(btnNewButton_2);
-
-        btnNewButton_3 = new JButton("New button");
-        panel.add(btnNewButton_3);
-
-        btnNewButton_4 = new JButton("New button");
-        panel.add(btnNewButton_4);
-
-        btnNewButton_5 = new JButton("New button");
-        panel.add(btnNewButton_5);
-
-        btnNewButton_6 = new JButton("New button");
-        panel.add(btnNewButton_6);
-
-        btnNewButton_7 = new JButton("New button");
-        panel.add(btnNewButton_7);
-
-        btnNewButton_8 = new JButton("New button");
-        panel.add(btnNewButton_8);
-
-        btnNewButton_12 = new JButton("New button");
-        panel.add(btnNewButton_12);
-
-        btnNewButton_13 = new JButton("New button");
-        panel.add(btnNewButton_13);
-
-        panel_test = new JPanel();
-        panel_test.setBounds(0, 0, 300, 600);
-        panel_test.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-
-        labelTitreForum = new JLabel("Titre de la Publication");
-        panel_test.add(labelTitreForum);
-
-        labelNbMsg = new JLabel("Nombre de message");
-        labelNbMsg.setBackground(SystemColor.textHighlight);
-        panel_test.add(labelNbMsg);
-
-        lblDerniermessage = new JLabel("Supprimer");
-        panel_test.add(lblDerniermessage);
-
-        panel.add(panel_test);
-        panel.add(panel_test);
-        panel.add(panel_test);
-        panel.add(panel_test);
-        panel.add(panel_test);
-        panel.add(panel_test);
-
-        btnNewButton_9 = new JButton("New button");
-        panel.add(btnNewButton_9);
-
-        btnNewButton_10 = new JButton("New button");
-        panel.add(btnNewButton_10);
-
-        btnNewButton_11 = new JButton("New button");
-        panel.add(btnNewButton_11);
+            panel.add ( new PanelMessage() );
 
 
 
-
-
+        }
 
 
     }
 
     private class PanelMessage extends JPanel {
 
+        private JLabel labelTitreForum;
+        private JLabel labelNbMsg;
+        private JLabel lblDerniermessage;
+
+
+        public PanelMessage() {
+
+
+            this.setBounds(0, 0, 300, 600);
+            this.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+            this.addMouseListener(ec);
+
+            labelTitreForum = new JLabel("Titre de la Publication");
+            this.add(labelTitreForum);
+
+            labelNbMsg = new JLabel("Nombre de message");
+            labelNbMsg.setBackground(SystemColor.textHighlight);
+            this.add(labelNbMsg);
+
+            lblDerniermessage = new JLabel("Supprimer");
+            this.add(lblDerniermessage);
+
+        }
+
+
+
+
+    }
+
+    private class Ecouteur implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent me) {
+
+            ForumView.this.setBackground(Color.RED);
+
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent arg0) {
+
+            ForumView.this.setBackground(null);
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent arg0) {
+            // TODO Auto-generated method stub
+
+        }
 
 
 
