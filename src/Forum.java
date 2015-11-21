3fe4765f672b9df7f5aa6073d11a678aaee0f385
alexandre <alex.boulay1@gmail.com>
2015-11-21 16:22:49 -0500

    Just a bit of work

--- src/Forum.java
+++ src/Forum.java
@@ -14,41 +14,73 @@ import com.mongodb.MongoClient;
 import com.mongodb.ParallelScanOptions;
 import com.mongodb.ServerAddress;
 
+import java.lang.annotation.Documented;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.text.Format;
 import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Locale;
+import com.mongodb.Block;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import com.mongodb.client.FindIterable;
+
+import javax.print.Doc;
 
 
 public class Forum {
 
-    DBCollection collectionDiscussions;
     MongoClient mongoClient;
-    DB forumDataBase;
+    MongoDatabase forumDataBase;
+    MongoCollection<Document> collectionDicussions;
+
 
     public Forum() throws Exception {
 
-        mongoClient = new MongoClient();
+        this.mongoClient = new MongoClient("127.0.0.1", 27017);
 
-        forumDataBase = mongoClient.getDB("forumDataBase");
+        this.forumDataBase = mongoClient.getDatabase("forumDataBase");
 
-        collectionDiscussions = forumDataBase.getCollection("forumCollection");
+        this.collectionDicussions = forumDataBase.getCollection("collectionMessages");
 
 
     }
 
-    public void addDiscution(String nomDuSujet,String message) {
+    public void addDiscution(String nomDuSujet,String message) throws ParseException {
 
-        BasicDBObject discussion = new BasicDBObject();
-        discussion.append("nomDuSujet",nomDuSujet);
-        discussion.append("message",message);
+        Document doc = new Document("nomDuSujet", nomDuSujet).
+                append("message", message).
+                append("date", this.getDate());
 
-
-        this.collectionDiscussions.insert(discussion);
+        this.collectionDicussions.insertOne(doc);
     }
 
     public void addMessage(){
 
+    }
 
+    public void test() {
 
+        FindIterable<Document> iterable = forumDataBase.getCollection("collectionMessages").find();
 
+        iterable.forEach(new Block<Document>() {
+            @Override
+            public void apply(final Document document) {
+                System.out.println(document);
+            }
+        });
+
+    }
+
+    public String getDate() throws ParseException {
+
+        Date aujourdhui = new Date();
+        DateFormat formatDeDate = new SimpleDateFormat("d MMM", Locale.CANADA_FRENCH);
+
+        return formatDeDate.format(aujourdhui);
     }
 
     /*FONCTION TEMPORAIRE MAIN*/
@@ -58,7 +90,13 @@ public class Forum {
         Forum ourForum = new Forum();
 
         ourForum.addDiscution("Bonjour", "LES POUBELLES CALISS, SORT LES!");
+        //System.out.println(ourForum.getDate());
+        //System.out.print("YO");
 
+
+        System.out.println(ourForum.getDate());
+
+        ourForum.test();
 
 
     }
---
+++ src/ForumView.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+/**
+ * Created by Alexandre on 2015-11-20.
+ */
+public class ForumView {
+    private JPanel panel1;
+}
